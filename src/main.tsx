acb2f0520166f9a67fcb6630201cf841f36a919a
bmj0053 <bmj05378@gmail.com>
2025-01-09 13:44:36 +0900

    chore: React Query, Axios 설치 및 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+      retry: 0,
+    },
+  },
+});
 
 async function enableMocking() {
   const { worker } = await import('./mocks/browser.js');
@@ -11,7 +22,9 @@ async function enableMocking() {
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </React.StrictMode>
   );
 });
